887679d83ac349f1a9c5fedfa09adf89a5752f5e
Peter Hristakos <peter.hristakos@bigpond.com>
2020-08-22 23:52:10 +1000

    Updates

---
+++ precache-manifest.08f47c637d9b6cb32930d48bf8e98ca2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520aac7d6c23f06ac7f15113adc371e5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f264d004bb995c07292e",
+    "url": "./static/css/2.afc36f9d.chunk.css"
+  },
+  {
+    "revision": "5d3a38043d4aada07f1e",
+    "url": "./static/css/main.ad397260.chunk.css"
+  },
+  {
+    "revision": "f264d004bb995c07292e",
+    "url": "./static/js/2.54a5e117.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.54a5e117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d3a38043d4aada07f1e",
+    "url": "./static/js/main.a588c691.chunk.js"
+  },
+  {
+    "revision": "321b7126446a202b5266",
+    "url": "./static/js/runtime-main.a9b2e859.js"
+  }
+]);
